bd6ffe140446ccd4ae54b86a053aa7b213e4b408
Hieu Van <viethieu972@gmail.com>
2024-08-26 23:07:06 +0700

    first

---
+++ src/components/PostList.jsx
@@ -0,0 +1,38 @@
+import Post from "./Post";
+import classes from "./PostList.module.css";
+import NewPost from "./NewPost";
+import { useState } from "react";
+import Modal from "./Modal";
+function PostList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHanlder(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
+  return (
+    <>
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHanlder} />
+        </Modal>
+      )}
+
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default PostList;
